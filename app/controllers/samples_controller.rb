e50b5b93ed52c54280a428aaee630c0aed67d693
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-09-19 16:06:35 +0100

    Add show, new, create method in smaplecontroller

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -7,4 +7,18 @@ class SamplesController < ApplicationController
     def new
         @sample = Sample.new
     end
+
+    def create
+        @sample = Sample.new(smaple_params)
+
+        if @sample.save
+            redirect_to sample_path(@sample)
+        else
+            render :new
+        end
+    end
+
+    def show
+        @sample = Sample.find(params[:id])
+    end
 end
