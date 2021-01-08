0239153778d2b0a28cc4d7f7fc1b7784d9ebe7a8
descholar-ceo <emmanuellamugi@gmail.com>
2021-01-08 18:46:46 +0200

    Configure registration with strong parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < ActionController::Base
   # def current_sample
   #   @current_sample = @sample.id
   # end
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name])
+  end
 end
