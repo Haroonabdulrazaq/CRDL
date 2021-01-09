f6b9f0d6a6f5e728271404aaf118bc9a1c8b7e22
descholar-ceo <emmanuellamugi@gmail.com>
2021-01-09 15:49:59 +0200

    Display create sample btn depending on the authorization

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,7 +14,7 @@ module ApplicationHelper
               <i class='fa fa-plus-circle is-3' arial-hidden='true'></i>
               </span>
               <span>New Sample</span>".html_safe
-            end}      
+            end if can? :manage, Sample}      
           </span>
       <span class='navbar-item'>#{link_to 'Logout', destroy_user_session_path, method: :delete, class: 'button is-danger'}</span>
       "
@@ -25,40 +25,6 @@ module ApplicationHelper
       "
     end
     display.html_safe
-  end
-
-  def display_create_result_button sample
-    display = ''
-    if can? :manage, Result
-      display = "
-      <nav class='navbar is-pulled-right'>
-        <div class='navbar-menu '>
-          <div class='navbar-item'>
-            <div class='field is-grouped '>
-              <p class='control '>
-                #{link_to 'Create Result', new_sample_result_path(sample), class: 'button is-dark'}
-              </p>
-          </div>
-        </div>
-      </nav>
-      "
-    end
-    display.html_safe
-  end
-
-  def display_sample_control_buttons sample
-    display = ''
-    if can? :manage, Sample
-      display = "
-        <div class='column'>
-          #{link_to 'Delete', sample_path, class: 'button is-danger is-outlined' , method: :delete , data: {confirm: 'Are you sure you want to delete this sample?'}}
-        </div>
-        <div class='column'>
-          #{link_to 'Edit', edit_sample_path(sample), class: 'button is-link is-outlined'}		
-        </div>
-      "
-    end 
-    display.html_safe   
   end
 
 end
--- app/helpers/samples_helper.rb
+++ app/helpers/samples_helper.rb
@@ -6,4 +6,39 @@ module SamplesHelper
                                    :supervisor_contact, :referrer, :current_status, :required_service,
                                    :description, :perishable, :retention_time, :result_due_date)
   end
+
+
+  def display_create_result_button sample
+    display = ''
+    if can? :manage, Result
+      display = "
+      <nav class='navbar is-pulled-right'>
+        <div class='navbar-menu '>
+          <div class='navbar-item'>
+            <div class='field is-grouped '>
+              <p class='control '>
+                #{link_to 'Create Result', new_sample_result_path(sample), class: 'button is-dark'}
+              </p>
+          </div>
+        </div>
+      </nav>
+      "
+    end
+    display.html_safe
+  end
+
+  def display_sample_control_buttons sample
+    display = ''
+    if can? :manage, Sample
+      display = "
+        <div class='column'>
+          #{link_to 'Delete', sample_path, class: 'button is-danger is-outlined' , method: :delete , data: {confirm: 'Are you sure you want to delete this sample?'}}
+        </div>
+        <div class='column'>
+          #{link_to 'Edit', edit_sample_path(sample), class: 'button is-link is-outlined'}		
+        </div>
+      "
+    end 
+    display.html_safe   
+  end
 end
