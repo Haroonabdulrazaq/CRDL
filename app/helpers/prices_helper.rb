206cc2ee05e5adbacbafc8fe43cbafa2887e4900
descholar-ceo <emmanuellamugi@gmail.com>
2021-01-12 10:57:55 +0200

    Debug the display_sample_prices_for_lab_tests method

--- app/helpers/prices_helper.rb
+++ app/helpers/prices_helper.rb
@@ -1,6 +1,6 @@
 
 module PricesHelper
-  def display_sample_prices price
-    
+  def display_sample_prices_for_lab_tests price
+    puts "This is the passed parameter #{price.lab_tests}"
   end
 end
