3b3ad565eaed1a0181f970e38b0aeb4b4e769a97
descholar-ceo <emmanuellamugi@gmail.com>
2021-01-14 13:05:57 +0200

    Display the total

--- app/helpers/prices_helper.rb
+++ app/helpers/prices_helper.rb
@@ -2,6 +2,7 @@ require 'json'
 module PricesHelper
   def display_sample_prices_for_lab_tests price
     price_rows = ''
+    total = 0
     unless price.nil? and price.lab_tests.size.zero?
       my_lab_tests = JSON.parse price.lab_tests
       my_lab_tests.each do |curr_lab_test|
@@ -10,7 +11,10 @@ module PricesHelper
             <td>#{curr_lab_test['title']}</td>
             <td>#{curr_lab_test['quantity']}</td>
             <td>#{curr_lab_test['unitPrice']}</td>
-            <td>#{curr_lab_test['unitPrice'] * curr_lab_test['quantity']}</td>
+            <td>#{
+              total += curr_lab_test['unitPrice'] * curr_lab_test['quantity']
+              curr_lab_test['unitPrice'] * curr_lab_test['quantity']
+              }</td>
           </tr>
         ".html_safe
       end
@@ -24,6 +28,16 @@ module PricesHelper
           <th>Subtotal</th>
         </tr>
         <tbody>#{price_rows}</tbody>
+      </table>
+      <table class='table is-bordered is-striped is-narrow is-hoverable is-fullwidth'>
+        <tr>
+          <th>Total</th>
+        </tr>
+        <tbody>
+          <tr>
+            <td>#{total}</td>
+          </tr>
+        </tbody>
       </table>
     ".html_safe
   end
