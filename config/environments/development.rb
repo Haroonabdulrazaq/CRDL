1068f15bf3a63ddf6821ab1a9fb8b271603a6cc1
descholar-ceo <emmanuellamugi@gmail.com>
2021-01-18 12:41:53 +0200

    Make the mailing works: Whenever a new result is available, the email is auto sent to the user

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,4 +63,17 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  host = 'localhost:3000' #replace with your own url
+  config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => 'descholar.stech@gmail.com',
+    :password             => 'mugirase',
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
 end
