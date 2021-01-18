27a412249a41b95dc79ad762538ee046a87bd277
descholar-ceo <emmanuellamugi@gmail.com>
2021-01-18 11:32:56 +0200

    Create email views

--- app/mailers/result_mailer.rb
+++ app/mailers/result_mailer.rb
@@ -1,2 +1,7 @@
 class ResultMailer < ApplicationMailer
+  def new_result_email
+    @result = params[:result]
+
+    mail(to: Sample.email, subject: "You got a new result on your sample!")
+  end
 end
