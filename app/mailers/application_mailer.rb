6f9850a63bfc69f46b92c7d49370ad5ffc447215
descholar-ceo <emmanuellamugi@gmail.com>
2021-01-18 11:17:08 +0200

    Add custom email address to application mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'descholar.stech@gmail.com'
   layout 'mailer'
 end
