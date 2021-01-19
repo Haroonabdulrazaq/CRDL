096905cd13877f3d869af1d2471cb6527b9a980c
descholar-ceo <emmanuellamugi@gmail.com>
2021-01-19 10:29:52 +0200

    Add new email form to the link

--- app/helpers/result_mails_helper.rb
+++ app/helpers/result_mails_helper.rb
@@ -3,11 +3,8 @@ module ResultMailsHelper
     display = ''
     if can? :manage, ResultMail
       display = "
-        <div class='column'>
-          #{link_to 'Delete', sample_result_path, class: 'button is-danger is-outlined' , method: :delete , data: {confirm: 'Are you sure you want to delete this sample?'}}
-        </div>
-        <div class='column'>
-          #{link_to 'Edit', edit_sample_result_path(result.sample.id, result), class: 'button is-link is-outlined'}		
+        <div class='column is-3 is-pulled-right'>
+          #{link_to 'Send email', new_result_mail_path, class: 'button is-primary is-outlined'}		
         </div>
       "
     end 
--- app/helpers/results_helper.rb
+++ app/helpers/results_helper.rb
@@ -6,10 +6,10 @@ module ResultsHelper
     display = ''
     if can? :manage, Result
       display = "
-        <div class='column is-5'>
+        <div class='column is-3'>
           #{link_to 'Delete', sample_result_path(result.sample.id, result), class: 'button is-danger is-outlined' , method: :delete , data: {confirm: 'Are you sure you want to delete this result?'}}
         </div>
-        <div class='column is-5 is-pulled-right'>
+        <div class='column is-3 is-pulled-right'>
           #{link_to 'Edit', edit_sample_result_path(result.sample.id, result), class: 'button is-link is-outlined'}		
         </div>
       "
