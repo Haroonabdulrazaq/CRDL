21613d34f2015c655b33039b4d3b1555b5c102d3
descholar-ceo <emmanuellamugi@gmail.com>
2021-01-19 21:57:18 +0200

    Send email

--- app/mailers/result_mailer.rb
+++ app/mailers/result_mailer.rb
@@ -1,7 +1,8 @@
 class ResultMailer < ApplicationMailer
   def new_result_email
     @email_params = params[:email_params]
+    @result = Result.find(@email_params[:result_id])
 
-    mail(to: @email_params.recipient_email, subject: "You got a new email from CRDL related to #{link_to 'this result', sample_result_url(email_params.result.sample_id, email_params.result)}!")
+    mail(to: @email_params[:recipient_email], subject: "You got a new email from CRDL related to result # #{@result.id} for sample: #{@result.sample.name}!")
   end
 end
