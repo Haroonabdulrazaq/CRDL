36cf951b2b49e02fcc8be6197c13c5cd08fd77b9
descholar-ceo <emmanuellamugi@gmail.com>
2021-01-19 10:18:41 +0200

    Resolve conflicts in this commit: Fix links issues on results

--- app/helpers/result_mails_helper.rb
+++ app/helpers/result_mails_helper.rb
@@ -1,2 +1,16 @@
 module ResultMailsHelper
+  def display_send_email_button result
+    display = ''
+    if can? :manage, ResultMail
+      display = "
+        <div class='column'>
+          #{link_to 'Delete', sample_result_path, class: 'button is-danger is-outlined' , method: :delete , data: {confirm: 'Are you sure you want to delete this sample?'}}
+        </div>
+        <div class='column'>
+          #{link_to 'Edit', edit_sample_result_path(result.sample.id, result), class: 'button is-link is-outlined'}		
+        </div>
+      "
+    end 
+    display.html_safe 
+  end
 end
