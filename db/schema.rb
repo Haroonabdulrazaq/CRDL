1b6cad66f7d5a58a57ca17af2a6fc5c8d24bfd49
descholar-ceo <emmanuellamugi@gmail.com>
2021-01-19 10:04:31 +0200

    Resolve conflicts from this committ: Remove scaffold stylings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_11_074443) do
+ActiveRecord::Schema.define(version: 2021_01_19_062611) do
 
   create_table "department_for_tests", force: :cascade do |t|
     t.string "title"
@@ -40,6 +40,16 @@ ActiveRecord::Schema.define(version: 2021_01_11_074443) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["sample_id"], name: "index_prices_on_sample_id"
+  end
+
+  create_table "result_mails", force: :cascade do |t|
+    t.string "recipient_email"
+    t.string "email_object"
+    t.text "email_message"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_result_mails_on_user_id"
   end
 
   create_table "results", force: :cascade do |t|
@@ -114,5 +124,6 @@ ActiveRecord::Schema.define(version: 2021_01_11_074443) do
   add_foreign_key "lab_tests", "department_for_tests"
   add_foreign_key "lab_tests", "users"
   add_foreign_key "prices", "samples"
+  add_foreign_key "result_mails", "users"
   add_foreign_key "results", "samples"
 end
