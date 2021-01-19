c3eb227040f912b7c813f6fcf2f4ab697bcb8647
descholar-ceo <emmanuellamugi@gmail.com>
2021-01-19 10:01:29 +0200

    Add validations to result_emails

--- app/models/result_mail.rb
+++ app/models/result_mail.rb
@@ -1,3 +1,9 @@
 class ResultMail < ApplicationRecord
+  #validations
+  validates :recipient_email, presence: true, length: { in: 3..50 }, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/
+  validates :email_object, presence: true, length: { in: 3..150 }
+  validates :email_message, presence: true, length: { in: 3..5000 }
+
+  #associations
   belongs_to :user
 end
